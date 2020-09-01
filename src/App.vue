46f28d215d6898aad4a2982845320a9a732cd4a1
panzhang <807773983@qq.com>
2020-09-01 17:54:03 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-      <da-button plain>按钮</da-button>
-      <da-button plain type="primary">primary按钮</da-button>
-      <da-button plain type="success">success按钮</da-button>
-      <da-button plain type="info">info按钮</da-button>
-      <da-button plain :circle="true" type="danger" icon="iconhuifu2" :disabled="true" @click="getInfo">danger按钮</da-button>
-      <da-button plain :round="true" type="warning" @click="getInfo">warning按钮</da-button>
+      <div class="item-list">
+        <da-button plain>按钮</da-button>
+        <da-button plain type="primary">primary按钮</da-button>
+        <da-button plain type="success">success按钮</da-button>
+        <da-button plain type="info">info按钮</da-button>
+        <da-button plain :circle="true" type="danger" icon="iconhuifu2" :disabled="true" @click="getInfo">danger按钮</da-button>
+        <da-button plain :round="true" type="warning" @click="getInfo">warning按钮</da-button>
+      </div>
+      <div class="item-list input-box">
+        <da-input v-model="form.username" :clearable="true" placeholder="请输入"></da-input>
+        <da-input v-model="form.password" type="password" :showPassword="true" placeholder="请输入"></da-input>
+      </div>
 
       <da-dialog :visible.sync="visible">
         <span slot="title">溫馨提示</span>
         <div>
-          這是內筒的
+          这是内容的
         </div>
         <div slot="footer">
           <da-button plain @click="getInfo">取消</da-button>
@@ -18,11 +24,22 @@
         </div>
       </da-dialog>
 
-      <div class="input-box">
-        <da-input v-model="form.username" :clearable="true" placeholder="请输入"></da-input>
-        <da-input v-model="form.password" type="password" :showPassword="true" placeholder="请输入"></da-input>
+      <div class="item-list">
         <da-switch name="switch" activeColor="#0099CC" inactiveColor="#99CCFF" v-model="form.active"></da-switch>
       </div>
+
+      <div class="item-list">
+         <da-radio name="radio" label="radio1" v-model="form.sex"></da-radio>
+        <da-radio name="radio2" label="radio2" v-model="form.sex"></da-radio>
+
+        <da-radio-group name="radiogroup" v-model="form.sex2">
+          <da-radio label="IPO" ></da-radio>
+          <da-radio label="业绩会" ></da-radio>
+          <da-radio label="媒体会" ></da-radio>
+        </da-radio-group>
+      </div>
+
+      <div class="test-div">circle</div>
   </div>
 </template>
 
@@ -34,7 +51,9 @@ export default {
       form:{
         username:"",
         password:"",
-        active:false
+        active:false,
+        sex:1,
+        sex2:1
       }
     }
   },
@@ -59,8 +78,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  text-align: center;
   .input-box {
     max-width: 300px;
+  }
+  .test-div {
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    color: #fff;
+    background-color: skyblue;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import DaButton from "./components/button.vue"
 import DaDialog from "./components/dialog.vue"
 import DaInput from "./components/input.vue"
 import DaSwitch from "./components/switch.vue"
+import DaRadio from "./components/radio.vue"
+import DaRadioGroup from "./components/radio-group.vue"
+
 
 Vue.config.productionTip = false
 
@@ -15,6 +18,8 @@ Vue.component(DaButton.name, DaButton);
 Vue.component(DaDialog.name, DaDialog);
 Vue.component(DaInput.name, DaInput);
 Vue.component(DaSwitch.name, DaSwitch);
+Vue.component(DaRadio.name, DaRadio);
+Vue.component(DaRadioGroup.name, DaRadioGroup);
 
 new Vue({
   router,
